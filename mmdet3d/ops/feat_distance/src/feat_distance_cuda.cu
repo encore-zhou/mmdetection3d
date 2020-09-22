56fa94081ebdd483972652ce7c584ddc85497d6d
zhoujiaming <zhoujiaming@sensetime.com>
2020-09-22 14:57:31 +0800

    modify feat distance cuda

--- mmdet3d/ops/feat_distance/src/feat_distance_cuda.cu
+++ mmdet3d/ops/feat_distance/src/feat_distance_cuda.cu
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define THREADS_PER_BLOCK 256
+#define THREADS_PER_BLOCK_SQRT 8
+#define THREADS_PER_BLOCK (THREADS_PER_BLOCK_SQRT * THREADS_PER_BLOCK_SQRT)
 #define DIVUP(m, n) ((m) / (n) + ((m) % (n) > 0))
 
 __global__ void feat_distance_kernel(int b, int n, int m, int c,
@@ -13,22 +14,20 @@ __global__ void feat_distance_kernel(int b, int n, int m, int c,
   // feat_b: (B, M, C)
   // output:
   //      distance: (B, N, M)
-  int bs_idx = blockIdx.y;
-  int feat_a_idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (bs_idx >= b || feat_a_idx >= n) return;
+  int bs_idx = blockIdx.z;
+  if (bs_idx >= b) return;
+  int feat_a_idx = blockIdx.y * blockDim.y + threadIdx.y;
+  int feat_b_idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (feat_a_idx >= n || feat_b_idx >= m) return;
 
   feat_a += bs_idx * n * c + feat_a_idx * c;
-  feat_b += bs_idx * m * c;
-  distance += bs_idx * n * m + feat_a_idx * m;
-
-  const float *cur_feat_b;
-
-  for (int i = 0; i < m; ++i) {
-    cur_feat_b = feat_b + i * c;
-    for (int j = 0; j < c; ++j) {
-      distance[i] += (feat_a[j] - cur_feat_b[j]) * (feat_a[j] - cur_feat_b[j]);
-    }
+  feat_b += bs_idx * m * c + feat_b_idx * c;
+  distance += bs_idx * n * m + feat_a_idx * m + feat_b_idx;
+  float dist_tmp = 0;
+  for (int j = 0; j < c; ++j) {
+    dist_tmp += (feat_a[j] - feat_b[j]) * (feat_a[j] - feat_b[j]);
   }
+  distance[0] = dist_tmp;
 }
 
 void feat_distance_kernel_launcher(int b, int n, int m, int c,
@@ -40,10 +39,9 @@ void feat_distance_kernel_launcher(int b, int n, int m, int c,
   //      distance: (B, N, M)
 
   cudaError_t err;
-
-  dim3 blocks(DIVUP(n, THREADS_PER_BLOCK),
-              b);  // blockIdx.x(col), blockIdx.y(row)
-  dim3 threads(THREADS_PER_BLOCK);
+  dim3 blocks(DIVUP(m, THREADS_PER_BLOCK_SQRT),
+              DIVUP(n, THREADS_PER_BLOCK_SQRT), b);
+  dim3 threads(THREADS_PER_BLOCK_SQRT, THREADS_PER_BLOCK_SQRT);
 
   feat_distance_kernel<<<blocks, threads, 0, stream>>>(b, n, m, c, feat_a,
                                                        feat_b, distance);
